310d7e230bf964de267e128de6bd5303c3280700
olecksandr.veleshchuk@digitalevo.io <olecksandr.veleshchuk@digitalevo.io>
2016-12-15 14:06:45 +0200

    fix clearing tet when user focus on send input

--- js/src/chatWindow.js
+++ js/src/chatWindow.js
@@ -3,15 +3,15 @@ let chatInput = document.querySelector('.send-input');
 // стирає напис "Введіть ваше повідомлення ..." коли коритувач перший раз клікає по
 // інпуту і після цього знімає обробник події, щоб наступного разу не стирати поідомлення 
 // користувача
-chatInput.addEventListener('focus', clearInputText);
+msgContentWrapper = document.querySelector('.ss-content');
+msgContentWrapper.addEventListener('focus', clearInputText);
 
 // if user click on send-input he do not drag the winow 
 chatInput.addEventListener('mousedown', stopPropagation);
 
 function clearInputText () {
-	content = document.querySelector('.ss-content');
-	content.innerHTML = '';
-	chatInput.removeEventListener('focus', clearInputText);
+	this.innerHTML = '';
+	this.removeEventListener('focus', clearInputText);
 }
 
 
